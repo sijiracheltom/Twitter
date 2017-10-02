e88141166833c59de7a41ebf463ca42a8580f1d0
Siji <rachtom89@gmail.com>
2017-10-01 18:59:10 -0700

    replyVC basics

---
+++ TwitterDemo/TwitterDemo/TweetReplyViewController.swift
@@ -0,0 +1,52 @@
+//
+//  TweetReplyViewController.swift
+//  TwitterDemo
+//
+//  Created by Siji Rachel Tom on 10/1/17.
+//  Copyright © 2017 Siji Rachel Tom. All rights reserved.
+//
+
+import UIKit
+
+class TweetReplyViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBAction func onCancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        textView.becomeFirstResponder()
+        textView.contentOffset = CGPoint.zero
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        textView.resignFirstResponder()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
