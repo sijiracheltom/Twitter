6fc6f0a63b594146c42422446d83164d04c2ce65
Siji <rachtom89@gmail.com>
2017-10-05 19:32:26 -0700

    Adding menu option table view

---
+++ TwitterDemo/TwitterDemo/MenuOptionCell.swift
@@ -0,0 +1,24 @@
+//
+//  MenuOptionCell.swift
+//  TwitterDemo
+//
+//  Created by Siji Rachel Tom on 10/5/17.
+//  Copyright © 2017 Siji Rachel Tom. All rights reserved.
+//
+
+import UIKit
+
+class MenuOptionCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TwitterDemo/TwitterDemo/MenuTableViewDataSource.swift
@@ -0,0 +1,29 @@
+//
+//  MenuTableViewDataSource.swift
+//  TwitterDemo
+//
+//  Created by Siji Rachel Tom on 10/5/17.
+//  Copyright © 2017 Siji Rachel Tom. All rights reserved.
+//
+
+import UIKit
+
+enum MenuOptions : Int {
+    case profile = 1
+    case timeLine
+    case mentions
+    case account
+    case totalCount
+}
+
+class MenuTableViewDataSource: NSObject, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MenuOptions.totalCount.rawValue
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuOptionCell", for: indexPath)
+        return cell
+    }
+
+}
